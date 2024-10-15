b3b36a4f046726e605c50f717939db61608c501b
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-10-15 19:58:51 -0300

    password verification addded

--- front/cadastro_login/script.js
+++ front/cadastro_login/script.js
@@ -7,3 +7,22 @@ const show_form = (show, hide) => {
    formHide.style.display = 'none'
    formHide.style.visibility = 'hidden'   
 }
+
+const verify_password = () => {
+   let password = document.getElementById('c-password')
+   let confPassword = document.getElementById('conf-password')
+
+   if(!(password.value == confPassword.value)) {
+      password.style.border = '1px solid var(--error)'
+      password.style.color = 'var(--error)'
+
+      confPassword.style.border = '1px solid var(--error)'
+      confPassword.style.color = 'var(--error)'
+   }else{
+      password.style.border = 'none'
+      password.style.color = 'var(--dark-gray)'
+
+      confPassword.style.border = 'none'
+      confPassword.style.color = 'var(--dark-gray)'
+   }
+}

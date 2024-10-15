8c69144cd010bdcf1fe1bf87d370e73927b2c762
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-10-15 02:25:10 -0300

    added smooth scroll, cards, text to section

--- front/chartjs.js
+++ front/chartjs.js
@@ -1,6 +1,6 @@
-const ctx = document.getElementById('freq-graph').getContext('2d')
+const ctx = document.getElementById('freq-graph')
 const ctx2 = document.getElementById('quantity-graph').getContext('2d')
-
+console.log(ctx.width)
 // Grafico de Top 10 paises com mais % da populacao que mais frequentam academias
 // ref: https://www.em.com.br/app/noticia/saude-e-bem-viver/2023/01/18/interna_bem_viver,1446196/brasileiros-estao-em-2-no-ranking-mundial-dos-que-mais-vao-a-academias.shtml
 const barGraph = new Chart(ctx, {
@@ -8,19 +8,19 @@ const barGraph = new Chart(ctx, {
   data: {
       labels: ['Índia', 'Brasil', 'África do Sul', 'China', 'Austrália', 'Suécia', 'Finlândia', 'México', 'Espanha', 'Reino Unido'],
       datasets: [{
-          label: '% da População na academia',
-          data: [24, 21, 21, 20, 19, 19, 15, 14, 14, 14],
-          backgroundColor: [
-              '#CB2D6F'
-          ],
+            label: '% da População na academia',
+            data: [24, 21, 21, 20, 19, 19, 15, 14, 14, 14],
+            backgroundColor: [
+               '#CB2D6F'
+            ],
          
       }]
   },
   options: {
       scales: {
-          y: {
-              beginAtZero: true
-          }
+         y: {
+            beginAtZero: true
+         }
       }
   }
 })
@@ -28,21 +28,21 @@ const barGraph = new Chart(ctx, {
 const barGraph2 = new Chart(ctx2, {
    type: 'bar', // Tipo de gráfico (ex: 'line', 'bar', 'pie')
    data: {
-       labels: ['Estados Unidos', 'Brasil', 'México', 'Alemanha', 'Argentina', 'Itália', 'Reino Unido', 'Coreia do Sul', 'Canadá', 'Japão'],
-       datasets: [{
-           label: 'Quantidade de academia',
-           data: [41.190, 29.525, 12.871, 9.669, 7.910, 7.760, 7.239, 6.590, 6.587, 4.950],
-           backgroundColor: [
-               '#13A098'
-           ],
-          
-       }]
+      labels: ['Estados Unidos', 'Brasil', 'México', 'Alemanha', 'Argentina', 'Itália', 'Reino Unido', 'Coreia do Sul', 'Canadá', 'Japão'],
+      datasets: [{
+         label: 'Quantidade de academia',
+         data: [41190, 29525, 12871, 9669, 7910, 7760, 7239, 6590, 6587, 4950],
+         backgroundColor: [
+            '#13A098'
+         ],
+         
+      }]
    },
    options: {
-       scales: {
-           y: {
-               beginAtZero: true
-           }
-       }
-   }
- })
+      scales: {
+         y: {
+            beginAtZero: true
+         }
+      }
+   },
+})

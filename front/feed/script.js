c5da5ca41dcc68afd932c1523ec6fc927fe4d065
phmpereira <pedro.pereira@sptech.school>
2024-10-21 10:16:10 -0300

    small changes on script.js from feed

--- front/feed/script.js
+++ front/feed/script.js
@@ -1,5 +1,4 @@
 const profileText = document.querySelector('#profile_text')
-const nameUser = localStorage.getItem('name_user')
 
 window.document.addEventListener('DOMContentLoaded', () => {
    profileText.innerHTML = `${localStorage.getItem('name_user')}`

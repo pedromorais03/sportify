10db0ea66678dfc384a8fafcb749b065fb65db94
phmpereira <pedro.pereira@sptech.school>
2024-10-21 10:45:33 -0300

    added check if localstorege is setted, redirect to feed page

--- front/script.js
+++ front/script.js
@@ -1,3 +1,9 @@
+window.document.addEventListener('DOMContentLoaded', () => {
+   if(localStorage.getItem('name_user')){
+      window.location.href = './feed/index.html'
+   }
+})
+
 const redirect_to = (page) =>{
    if(page){
       window.location.href = `./${page}/index.html`

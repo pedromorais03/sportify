ae5f873441df6a20dee81e51eb5a3b744ce27bcc
phmpereira <pedro.pereira@sptech.school>
2024-10-24 12:16:16 -0300

    created profile options and logout

--- front/feed/script.js
+++ front/feed/script.js
@@ -1,7 +1,23 @@
 const profileText = document.querySelector('#profile_text')
+const profile = document.querySelector('.profile')
+const profileOption = document.querySelector('.profile-option')
 
 window.document.addEventListener('DOMContentLoaded', () => {
    profileText.innerHTML = `${localStorage.getItem('name_user')}`
    // console.log(nameUser)
 })
+
+profile.addEventListener('click', () => {
+   if(getComputedStyle(profileOption).display == 'none'){
+      profileOption.style.display = 'block'
+   }else{
+      profileOption.style.display = 'none'
+   }
+   
+})
+
+const logout = () => {
+   localStorage.clear()
+   window.location.href = '../index.html'
+}
 

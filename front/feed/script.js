df1e327da6714843fa821e01086c46838373675d
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-10-24 21:30:56 -0300

    marked and stylezed modal and toast to feed page

--- front/feed/script.js
+++ front/feed/script.js
@@ -2,12 +2,18 @@ const profileText = document.querySelector('#profile_text')
 const profile = document.querySelector('.profile')
 const profileOption = document.querySelector('.profile-option')
 
+const toast = document.querySelector('.toast')
+const toastText = document.querySelector('.toast-text')
+const toastTimer = document.querySelector('.toast-timer')
+
+const postModal = document.querySelector('#modal-post')
+
 window.document.addEventListener('DOMContentLoaded', () => {
-   if(localStorage.getItem('name_user')) {
-      profileText.innerHTML = `${localStorage.getItem('name_user')}`
-   }else{
-      window.location.href = '../index.html'
-   }
+   // if(localStorage.getItem('name_user')) {
+   //    profileText.innerHTML = `${localStorage.getItem('name_user')}`
+   // }else{
+   //    window.location.href = '../index.html'
+   // }
 })
 
 profile.addEventListener('click', () => {
@@ -19,8 +25,16 @@ profile.addEventListener('click', () => {
    
 })
 
+const show_post_modal = () => {
+   postModal.style.display = 'flex'
+}
+
+const close_post_modal = () => {
+   postModal.style.display = 'none'
+}
+
+
 const logout = () => {
    localStorage.clear()
    window.location.href = '../index.html'
 }
-

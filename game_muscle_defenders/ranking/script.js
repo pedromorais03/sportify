1a276deed5d4b78eb0bf7edc96bf5c5cb9b583ee
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-11-25 01:13:20 -0300

    added css and fixed some js bugs in ranking page

--- game_muscle_defenders/ranking/script.js
+++ game_muscle_defenders/ranking/script.js
@@ -1,6 +1,27 @@
-const containerTitle = document.querySelector('.container-title')
+// const containerTitle = document.querySelector('.container-title')
+const profileText = document.querySelector('#profile_text')
+const profile = document.querySelector('.profile')
+const profileOption = document.querySelector('.profile-option')
 const nameUser = localStorage.getItem('name_user')
 
-window.addEventListener('DOMContentLoaded', () => {
-   containerTitle.innerHTML = `${nameUser}`
+window.document.addEventListener('DOMContentLoaded', () => {
+   if(localStorage.getItem('name_user')){
+      profileText.innerHTML = `${localStorage.getItem('name_user')}`
+   }else{
+      window.location.href = '../index.html'
+   }
 })
+
+profile.addEventListener('click', () => {
+   if(getComputedStyle(profileOption).display == 'none'){
+      profileOption.style.display = 'block'
+   }else{
+      profileOption.style.display = 'none'
+   }
+   
+})
+
+const logout = () => {
+   localStorage.clear()
+   window.location.href = '../index.html'
+}

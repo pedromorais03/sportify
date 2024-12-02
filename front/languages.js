d43f3f2e19fff93a27f506796ccf8617515ac707
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-12-01 22:44:29 -0300

    index page translated

--- front/languages.js
+++ front/languages.js
@@ -5,7 +5,23 @@ const languages = {
       banner: 'Sua jornada começa aqui',
       banner_button: 'Cadastre-se',
       discover_title: 'Conheça a Sportify',
-      discover_text: 'A Sportify é uma rede social dedicada à musculação e esportes, criada para conectar entusiastas de todas as idades e níveis de experiência. Nossa plataforma oferece um espaço acolhedor onde os usuários podem compartilhar treinos, trocar dicas, acompanhar seu progresso e encontrar motivação em uma comunidade vibrante e inspiradora. Seja você um iniciante ou um atleta experiente, a Sportify é o lugar perfeito para alcançar seus objetivos de forma saudável e eficiente.'
+      discover_text: 'A Sportify é uma rede social dedicada à musculação e esportes, criada para conectar entusiastas de todas as idades e níveis de experiência. Nossa plataforma oferece um espaço acolhedor onde os usuários podem compartilhar treinos, trocar dicas, acompanhar seu progresso e encontrar motivação em uma comunidade vibrante e inspiradora. Seja você um iniciante ou um atleta experiente, a Sportify é o lugar perfeito para alcançar seus objetivos de forma saudável e eficiente.',
+      mission_title: 'Missão',
+      mission_text: 'Promover um ambiente inclusivo e motivador para entusiastas de musculação e esportes, facilitando a troca de conhecimentos e experiências que impulsionam o crescimento pessoal e físico de nossos usuários.',
+      vision_title: 'Visão',
+      vision_text: 'Ser a principal rede social para amantes de musculação e esportes, reconhecida por sua comunidade engajada e pelas ferramentas inovadoras que ajudam os usuários a atingir seus objetivos e melhorar sua qualidade de vida.',
+      values_title: 'Valores',
+      values_text1: 'Inclusão: Valorizamos a diversidade e acolhemos todos os indivíduos, independentemente de seu nível de experiência.',
+      values_text2: 'Colaboração: Acreditamos na força da comunidade e incentivamos a troca de conhecimento e apoio mútuo.',
+      values_text3: 'Saúde: Promovemos um estilo de vida saudável, priorizando o bem-estar físico e mental de nossos usuários.',
+      who_is: 'Pra quem é a Sportify?',
+      who_is_text: ` A Sportify é uma rede social para todos que amam musculação e esportes, desde iniciantes até atletas avançados. É um espaço para compartilhar treinos, acompanhar progressos e se conectar com pessoas com interesses semelhantes.
+                     <br>
+                     Além disso, a plataforma beneficia treinadores e profissionais, permitindo a expansão de redes de contatos e a oferta de orientações personalizadas. Com uma comunidade de apoio, a Sportify ajuda todos a alcançar seus objetivos de forma saudável.`,
+      graph_title: 'Gráfico',
+      graph1_label: '% da População frequentando academias',
+      graph2_label: 'Quantidade de academias abertas',
+      footer: 'Todos direitos reservados para Sportify<sup>©</sup> - 2024'
    },
    en: {
       header_item1: 'Discover Sportify',
@@ -13,6 +29,22 @@ const languages = {
       banner: 'Your journey begins here',
       banner_button: 'Sign up',
       discover_title: 'Discover Sportify',
-      discover_text: `Sportify is a social network dedicated to bodybuilding and sports, created to connect enthusiasts of all ages and experience levels. Our platform provides a welcoming space where users can share workouts, exchange tips, track their progress and find motivation in a vibrant and inspiring community. Whether you're a beginner or an experienced athlete, Sportify is the perfect place to reach your goals in a healthy and efficient way.`
+      discover_text: `Sportify is a social network dedicated to bodybuilding and sports, created to connect enthusiasts of all ages and experience levels. Our platform provides a welcoming space where users can share workouts, exchange tips, track their progress and find motivation in a vibrant and inspiring community. Whether you're a beginner or an experienced athlete, Sportify is the perfect place to reach your goals in a healthy and efficient way.`,
+      mission_title: 'Mission',
+      mission_text: 'Promote an inclusive and motivating environment for bodybuilding and sports enthusiasts, facilitating the exchange of knowledge and experiences that boost the personal and physical growth of our users.',
+      vision_title: 'Vision',
+      vision_text: `To be the main social network for bodybuilding and sports lovers, recognized for its engaged community and innovative tools that help users achieve their goals and improve their quality of life.`,
+      values_title: 'Values',
+      values_text1: 'Inclusion: We value diversity and welcome all individuals, regardless of their level of experience.',
+      values_text2: 'Collaboration: We believe in the strength of the community and encourage knowledge sharing and mutual support.',
+      values_text3: 'Health: We promote a healthy lifestyle, prioritizing the physical and mental well-being of our users.',
+      who_is: 'Who is it for?',
+      who_is_text: `Sportify is a social network for everyone who loves bodybuilding and sports, from beginners to advanced athletes. It’s a space to share workouts, track progress, and connect with people with similar interests.  
+                    <br>  
+                    Additionally, the platform benefits trainers and professionals, enabling them to expand their networks and offer personalized guidance. With a supportive community, Sportify helps everyone achieve their goals in a healthy way.`,
+      graph_title: 'Graphics',
+      graph1_label: '% of the Population Attending Gyms',
+      graph2_label: 'Number of Gyms Opened',
+      footer: 'All rights reserved to Sportify<sup>©</sup> - 2024'
    }
 }
--- front/script.js
+++ front/script.js
@@ -57,4 +57,21 @@ const set_texts = () => {
    header_item2.innerText += `${languages[language].header_item2}`
    discover_section.innerText += `${languages[language].discover_title}`
    discover_text.innerText += `${languages[language].discover_text}`
+   mission_title.innerText += `${languages[language].mission_title}`
+   mission_text.innerText += `${languages[language].mission_text}`
+   vision_title.innerText += `${languages[language].vision_title}`
+   vision_text.innerText += `${languages[language].vision_text}`
+   values_title.innerText += `${languages[language].values_title}`
+   values_text1.innerText += `${languages[language].values_text1}`
+   values_text2.innerText += `${languages[language].values_text2}`
+   values_text3.innerText += `${languages[language].values_text3}`
+   who_is.innerText += `${languages[language].who_is}`
+   who_is_text.innerHTML += `${languages[language].who_is_text}`
+   graph_title.innerText += `${languages[language].graph_title}`
+   graph1.innerText += `${languages[language].graph1_label}`
+   graph2.innerText += `${languages[language].graph2_label}`
+   footer.innerHTML += `${languages[language].footer}`
+   footer_link1.innerText += `${languages[language].header_item1}`
+   footer_link2.innerText += `${languages[language].header_item2}`
+   footer_link3.innerText += `${languages[language].banner_button}`
 }

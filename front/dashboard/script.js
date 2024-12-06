c3b23bf0b0cd9ea840fef149e026eb4e83ef0f74
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-12-05 22:08:19 -0300

    added slide section to dashboard page

--- front/dashboard/script.js
+++ front/dashboard/script.js
@@ -1,6 +1,10 @@
 const profileText = document.querySelector('#profile_text')
 const profile = document.querySelector('.profile')
 const profileOption = document.querySelector('.profile-option')
+const slides = document.querySelector('.slider-slides')
+const changeButton = document.getElementById('toggleButton')
+
+let currentSection = 0
 
 window.document.addEventListener('DOMContentLoaded', () => {
    if(localStorage.getItem('name_user')) {
@@ -17,6 +21,11 @@ profile.addEventListener('click', () => {
       profileOption.style.display = 'none'
    }
    
+})
+
+changeButton.addEventListener('click', () => {
+   currentSection = (currentSection + 1) % 2
+   slides.style.transform = `translateX(-${currentSection * 100}%)`
 })
 
 const logout = () => {

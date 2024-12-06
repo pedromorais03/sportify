098d8c3da80a985bb6f4cd129bebc47a53a1d7ab
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-12-06 01:54:37 -0300

    added sql scripts with changes

--- sql/sportify_routines.sql
+++ sql/sportify_routines.sql
@@ -128,6 +128,30 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `p_insert_game_interaction` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`pedro_morais`@`%` PROCEDURE `p_insert_game_interaction`(
+	IN score DECIMAL(10,2),
+    IN id_user INT,
+    IN id_game INT
+)
+BEGIN
+   INSERT INTO run_data VALUES (default, score, id_user, id_game, default);
+   INSERT INTO interaction VALUES (default, 'game', id_user, default);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `p_insert_post_interaction` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -171,7 +195,7 @@ CREATE DEFINER=`pedro_morais`@`%` PROCEDURE `p_insert_recipe_interaction`(
 )
 BEGIN
    INSERT INTO recipes VALUES (default, recipe_title, recipe_ingredients, recipe_description, recipe_prep_method, id_user, default);
-   INSERT INTO interaction VALUES (default, 'recipe', id_user);
+   INSERT INTO interaction VALUES (default, 'recipe', id_user, default);
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -188,4 +212,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-28 22:54:57
+-- Dump completed on 2024-12-06  1:54:09
